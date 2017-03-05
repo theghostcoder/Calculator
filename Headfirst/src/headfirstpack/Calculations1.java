eb01537946e62f4f7e79e2dd2751ee7015af0415
theghostcoder <anamikaiec@gmail.com>
2017-03-04 18:23:21 -0600

    add modify

---
+++ Headfirst/src/headfirstpack/Calculations1.java
@@ -0,0 +1,45 @@
+package headfirstpack;
+
+public class Calculations {// access identifier,class,classname
+
+	public int addNum(int a, int b) {// access identifier,return type,method
+										// name
+		// int a,b are parameters
+		int sum;
+		sum = a + b;
+		return sum;
+		//no printing of value, just returning values
+
+	}
+
+	public void subNum(int c, int d) {
+
+		int sub;
+		sub = c - d;
+		System.out.println(sub);
+	}
+
+	public int mulNum(int e, int f) {
+
+		int mul = e * f;
+		return mul;
+	}
+
+	public int divNum(int g, int h) {
+
+		int div = g / h;
+		return div;
+
+	}
+
+	public int remainNum(int i, int j) {
+
+		int remain = i % j;
+		return remain;
+	}
+	public void calcarea(float l,float b){
+		float area = l*b;
+		System.out.println("this is our area" + area);
+}
+
+}
